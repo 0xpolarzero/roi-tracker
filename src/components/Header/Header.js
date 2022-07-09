07e7c8938af4ce0327c35b57282780356a99df04
polar0 <23r0eth@gmail.com>
2022-07-09 18:23:32 +0200

    Change app title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,8 +10,11 @@ const Header = ({ web3, ethPriceValue }) => {
     <header className='header'>
       <div className='nav'>
         <div className='app-title'>
-          <span className='alt-char'>0x</span>
-          <span className='main-char'>Tracer</span>
+          <span className='content'>
+            <span className='alt-char'>polar</span>
+            <span className='main-char'>eth</span>
+          </span>
+          <i className='fa-solid fa-asterisk'></i>
         </div>
         <EthPrice ethPriceValue={ethPriceValue} />
         <Profile />
