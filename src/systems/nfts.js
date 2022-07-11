173ee8058372785f1b7d9dbebd92333115cd87b6
polar0 <23r0eth@gmail.com>
2022-07-11 09:54:34 +0200

    Get NFT transactions with alchemy

--- src/systems/nfts.js
+++ src/systems/nfts.js
@@ -1,25 +1,28 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+
+import { displayNotif } from './utils';
+
+const web3 = createAlchemyWeb3(`https://eth-mainnet.alchemyapi.io/v2/demo`);
+
 async function getNftExchanges(provider, startBlock, endBlock, addresses) {
-  const currentNfts = await getCurrentNfts(provider, addresses);
+  const currentNfts = await getCurrentNfts(provider, addresses).catch((err) => {
+    console.log(err);
+    displayNotif('error', 'We could not load your current NFTs.', 4000);
+  });
 
-  // const transactions = await getNftTransactions(
-  //   provider,
-  //   startBlock,
-  //   endBlock,
-  //   addresses,
-  // );
-
-  console.log(JSON.parse(currentNfts[0].metadata));
-}
-
-async function getNftTransactions(provider, startBlock, endBlock, addresses) {
-  for (const address of addresses) {
-    // const exchangesAddress = exchangesAll.result.filter(
-    // exchanges.push(...exchangeForAddress.result);
-    // exchanges.in.push(...exchangesInForAddress.transfers);
-    // exchanges.out.push(...exchangesOutForAddress.transfers);
-    // return exchanges;
-  }
-  // return exchangesAll;
+  const transactions = await getNftTransactions(
+    startBlock,
+    endBlock,
+    addresses,
+  ).catch((err) => {
+    console.log(err);
+    displayNotif(
+      'error',
+      'There was an issue fetching the NFT exchanges with your addresses.',
+      4000,
+    );
+  });
+  console.log(transactions);
 }
 
 async function getCurrentNfts(provider, addresses) {
@@ -33,6 +36,37 @@ async function getCurrentNfts(provider, addresses) {
   }
 
   return currentNfts;
+}
+
+async function getNftTransactions(startBlock, endBlock, addresses) {
+  let exchanges = [];
+
+  for (const address of addresses) {
+    const exchangesInForAddress = await web3.alchemy.getAssetTransfers({
+      fromBlock: startBlock.block,
+      toBlock: endBlock.block,
+      toAddress: address,
+      order: 'desc',
+      category: ['erc721', 'erc1155'],
+      withMetaData: true,
+    });
+
+    const exchangesOutForAddress = await web3.alchemy.getAssetTransfers({
+      fromBlock: startBlock.block,
+      toBlock: endBlock.block,
+      fromAddress: address,
+      order: 'desc',
+      category: ['erc721', 'erc1155'],
+      withMetaData: true,
+    });
+
+    exchanges.push(
+      ...exchangesInForAddress.transfers,
+      ...exchangesOutForAddress.transfers,
+    );
+
+    return exchanges;
+  }
 }
 
 export { getNftExchanges };
